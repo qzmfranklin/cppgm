e3465daaa9fb0d31c504b68f01cbafcf1559960c
Zhongming Qu <qzmfranklin@gmail.com>
2015-11-18 20:26:19 -0800

    Add pa1/UTF32Stream

---
+++ pa1/PPUTF32Stream.h
@@ -0,0 +1,25 @@
+#ifndef PPUTF32Stream_h
+#define PPUTF32Stream_h
+
+#include "UTF32StreamIfc.h"
+#include "unicode/ucnv.h"
+#include "unicode/schriter.h"
+#include <string>
+
+class PPUTF32Stream: public UTF32StreamIfc {
+public:
+  PPUTF32Stream(const std::string&);
+
+  virtual bool isEmpty() const override;
+  virtual char32_t getChar32() const override;
+  virtual void toNext() override;
+
+  virtual std::u32string getUTF32String() const override;
+  virtual std::string getUTF8String() const override;
+
+private:
+  icu::UnicodeString _str;
+  mutable icu::StringCharacterIterator _itr;
+};
+
+#endif /* end of include guard */
