83976ec10ce47f909739e61129b8f6dfcd9b2767
Zhongming Qu <qzmfranklin@gmail.com>
2015-11-19 17:39:06 -0800

    Add an empty pa1/gtest_PPCodePointCheck.cpp

---
+++ pa1/gtest_PPCodePointCheck.cpp
@@ -0,0 +1,62 @@
+#include "PPCodePointCheck.h"
+#include <gtest/gtest.h>
+
+TEST(PPCodePointCheck, isBasicSourceCharacter)
+{
+}
+
+TEST(PPCodePointCheck, isDigit)
+{
+}
+
+TEST(PPCodePointCheck, isNonDigit)
+{
+}
+
+TEST(PPCodePointCheck, isInAnnexE1)
+{
+}
+
+TEST(PPCodePointCheck, isInAnnexE2)
+{
+}
+
+TEST(PPCodePointCheck, isBinaryDigit)
+{
+}
+
+TEST(PPCodePointCheck, isOctalDigit)
+{
+}
+
+TEST(PPCodePointCheck, isHexadecimalDigit)
+{
+}
+
+TEST(PPCodePointCheck, isSimpleEscapeChar)
+{
+}
+
+TEST(PPCodePointCheck, isSign)
+{
+}
+
+TEST(PPCodePointCheck, isNotHChar)
+{
+}
+
+TEST(PPCodePointCheck, isNotQChar)
+{
+}
+
+TEST(PPCodePointCheck, isNotSChar)
+{
+}
+
+TEST(PPCodePointCheck, isNotRChar)
+{
+}
+
+TEST(PPCodePointCheck, isNotDChar)
+{
+}
