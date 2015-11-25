9beb6bd5bc7c01eba311cdf6948a57ad718dcd5a
Zhongming Qu <qzmfranklin@gmail.com>
2015-11-24 17:16:54 -0800

    PPCodeUnitStream: Prepare for adding digraph in the next commit. Passed existing tests.

--- pa1/PPCodeUnitStream.cpp
+++ pa1/PPCodeUnitStream.cpp
@@ -1,22 +1,23 @@
 #include "PPCodePointCheck.h"
 #include "PPCodeUnitStream.h"
-#include <string>
+#include <assert.h>
 
 
 PPCodeUnitStream::PPCodeUnitStream(std::shared_ptr<UTF32StreamIfc> u32stream):
-  _u32stream(u32stream), _curr(nullptr)
+  _u32stream(u32stream)
 {
-  this->toNext();
+  _pushCodeUnits();
 }
 
 bool PPCodeUnitStream::isEmpty() const
 {
-  return _u32stream->isEmpty()  &&  (_curr == nullptr);
+  return _u32stream->isEmpty()  &&  _queue.empty();
 }
 
 const std::shared_ptr<PPCodeUnit> PPCodeUnitStream::getCodeUnit() const
 {
-  return _curr;
+  assert(!_queue.empty());
+  return _queue.front();
 }
 
 std::string PPCodeUnitStream::getErrorMessage() const
@@ -26,7 +27,6 @@ std::string PPCodeUnitStream::getErrorMessage() const
 
 void PPCodeUnitStream::_setError(const std::string &&u8str)
 {
-  fprintf(stderr,"%s\n", u8str.c_str());
   _errorMessage = std::move(u8str);
 }
 
@@ -36,6 +36,14 @@ void PPCodeUnitStream::_clearError()
 }
 
 void PPCodeUnitStream::toNext()
+{
+  assert(!_queue.empty());
+  _queue.pop();
+  if (_queue.empty())
+    _pushCodeUnits();
+}
+
+void PPCodeUnitStream::_pushCodeUnits()
 {
   enum class State {
     Start,
@@ -47,174 +55,200 @@ void PPCodeUnitStream::toNext()
     MultipleLineComment,
     MultipleLineCommentStar,
 
+    LeftParenthesis,
+    Column,
+    PercentSign,
+    PercentSign2,
+    PercentSign3,
+
     End,
     Error
   };
-
-  std::shared_ptr<PPCodeUnit> unit = nullptr;
 
   std::u32string u32str;
   State state = State::Start;
   _clearError();
   while(state != State::End  &&  state != State::Error) {
-    if (_u32stream->isEmpty()) {
-      _curr = nullptr;
+    if (_u32stream->isEmpty())
       break;
-    }
 
     const char32_t curr32 = _u32stream->getChar32();
     _u32stream->toNext();
 
-    switch (state) {
-
-      case State::Start:
-        if (curr32 == U'\\') { // Line splicing, universal-character-name
-          const char32_t next32 = _u32stream->getChar32();
-          if (next32 == U'\n') { // Line splicing
-            // Pass
-          } else if (next32 == U'u') { // \uXXXX
-            state = State::SingleQuad;
-          } else if (next32 == U'U') { // \UXXXXXXXX
-            state = State::DoubleQuad;
-          } else {
-            state = State::Error;
-            _setError(R"(Illegal character following \)");
-          }
-          _u32stream->toNext();
-        } else if (curr32 == U'/') { // Comments
-          const char32_t next32 = _u32stream->getChar32();
-          if (next32 == U'/') { // Single-line comment
-            state = State::SingleLineComment;
-          } else if (next32 == U'*') { // Multiple-line comment
-            state = State::MultipleLineCommentStart;
-          } else {
-            state = State::Error;
-            _setError(R"(Illegal character following /)");
-          }
-          _u32stream->toNext();
-        } else if (PPCodePointCheck::isBasicSourceCharacter(curr32)) {
-          _curr = PPCodeUnit::createASCIIChar(static_cast<const char>(curr32));
-          state = State::End;
+    if (state == State::Start) {
+      if (curr32 == U'\\') { // Line splicing, universal-character-name
+        const char32_t next32 = _u32stream->getChar32();
+        if (next32 == U'\n') { // Line splicing
+          // Pass
+        } else if (next32 == U'u') { // \uXXXX
+          state = State::SingleQuad;
+        } else if (next32 == U'U') { // \UXXXXXXXX
+          state = State::DoubleQuad;
         } else {
-          _setError(R"(Not a basic-source-character)");
           state = State::Error;
+          _setError(R"(Illegal character following \)");
         }
-        break;
-
-      case State::SingleQuad:
-        {
-          std::string u8str(1, static_cast<char>(curr32));
-          for (int i = 0; i < 3; i++) {
-            const char32_t ch32 = _u32stream->getChar32();
-            _u32stream->toNext();
-            if (PPCodePointCheck::isHexadecimalDigit(ch32)) {
-              u8str  += static_cast<char>(ch32);
-            } else {
-              state = State::Error;
-              _setError(R"(Illegal character following \u: )" + std::string(1, static_cast<char>(ch32)));
-            }
-          }
-          const char32_t value = static_cast<char32_t>(std::stoull(u8str, nullptr, 16));
-          _curr = PPCodeUnit::createUniversalCharacterName(value, std::string("\\u") + u8str);
-          state = State::End;
+        _u32stream->toNext();
+      } else if (curr32 == U'/') { // Comments
+        const char32_t next32 = _u32stream->getChar32();
+        if (next32 == U'/') { // Single-line comment
+          state = State::SingleLineComment;
+        } else if (next32 == U'*') { // Multiple-line comment
+          state = State::MultipleLineCommentStart;
+        } else {
+          state = State::Error;
+          _setError(R"(Illegal character following /)");
         }
-        break;
-
-      case State::DoubleQuad:
-        {
-          std::string u8str(1, static_cast<char>(curr32));
-          for (int i = 0; i < 7; i++) {
-            const char32_t ch32 = _u32stream->getChar32();
-            _u32stream->toNext();
-            if (PPCodePointCheck::isHexadecimalDigit(ch32)) {
-              u8str  += static_cast<char>(ch32);
-            } else {
-              state = State::Error;
-              _setError(R"(Illegal character following \U)" + std::string(1, static_cast<char>(ch32)));
-            }
-          }
-          const char32_t value = static_cast<char32_t>(std::stoull(u8str, nullptr, 16));
-          _curr = PPCodeUnit::createUniversalCharacterName(value, std::string("\\U") + u8str);
-
-          state = State::End;
-        }
-        break;
-
-      case State::SingleLineComment:
-        {
-          u32str = U"//";
-          u32str += curr32;
-          while(1) {
-            if (_u32stream->isEmpty()) {
-              state = State::Error;
-              _setError(R"(Reached end-of-file in single-line-comment)");
-              _curr = nullptr;
-              break;
-            }
-
-            const char32_t ch32 = _u32stream->getChar32();
-            if (ch32 == U'\n') {
-              _curr = PPCodeUnit::createComment(u32str);
-              state = State::End;
-              break;
-            } else {
-              u32str += ch32;
-              _u32stream->toNext();
-            }
-          }
-        }
-        break;
-
-      case State::MultipleLineCommentStart:
-        // *      =>  MultipleLineComment
-        // other  =>  Error
-        {
-          u32str = U"/*";
-          u32str += curr32;
-          state = State::MultipleLineComment;
-        }
-        break;
-
-      case State::MultipleLineComment:
-        // *      =>  MultipleLineCommentStar
-        // other  =>  MultipleLineComment
-        {
-          u32str += curr32;
-          if (curr32 == U'*')
-            state = State::MultipleLineCommentStar;
-        }
-        break;
-
-      case State::MultipleLineCommentStar:
-        // *      =>  MultipleLineCommentStar
-        // /      =>  End
-        // other  =>  MultipleLineComment
-        {
-          u32str += curr32;
-
-          if (curr32 == U'*') {
-            break;
-          } else if (curr32 == U'/') {
-            state = State::End;
-            _curr = PPCodeUnit::createComment(u32str);
-          } else {
-            state = State::MultipleLineComment;
-            continue;
-          }
-        }
-        break;
-
-      case State::End:
+        _u32stream->toNext();
+      } else if (curr32 == U'<') { // Possibly <%, <:
+        state = State::LeftParenthesis;
+      } else if (curr32 == U':') { // Possibly :>
+        state = State::Column;
+      } else if (curr32 == U'%') { // Possibly %> %: %:%:
+        state = State::PercentSign;
+      } else if (PPCodePointCheck::isBasicSourceCharacter(curr32)) {
+        _queue.push(PPCodeUnit::createASCIIChar(static_cast<const char>(curr32)));
+        state = State::End;
+      } else {
+        _setError(R"(Not a basic-source-character)");
         state = State::Error;
-        _setError(R"(Reached an impossible state State::End)");
-        break;
-
-      case State::Error:
-        state = State::Error;
-        _setError(R"(Reached an impossible state State::Error)");
-        break;
+      }
     }
 
-    //fprintf(stderr,"}\n", static_cast<uint32_t>(curr32));
+    else if (state == State::SingleQuad) {
+      std::string u8str(1, static_cast<char>(curr32));
+      for (int i = 0; i < 3; i++) {
+        const char32_t ch32 = _u32stream->getChar32();
+        _u32stream->toNext();
+        if (PPCodePointCheck::isHexadecimalDigit(ch32)) {
+          u8str  += static_cast<char>(ch32);
+        } else {
+          state = State::Error;
+          _setError(R"(Illegal character following \u: )" + std::string(1, static_cast<char>(ch32)));
+        }
+      }
+      const char32_t value = static_cast<char32_t>(std::stoull(u8str, nullptr, 16));
+      _queue.push(PPCodeUnit::createUniversalCharacterName(value, std::string("\\u") + u8str));
+      state = State::End;
+    }
+
+    else if (state == State::DoubleQuad) {
+      std::string u8str(1, static_cast<char>(curr32));
+      for (int i = 0; i < 7; i++) {
+        const char32_t ch32 = _u32stream->getChar32();
+        _u32stream->toNext();
+        if (PPCodePointCheck::isHexadecimalDigit(ch32)) {
+          u8str  += static_cast<char>(ch32);
+        } else {
+          state = State::Error;
+          _setError(R"(Illegal character following \U)" + std::string(1, static_cast<char>(ch32)));
+        }
+      }
+      const char32_t value = static_cast<char32_t>(std::stoull(u8str, nullptr, 16));
+      _queue.push(PPCodeUnit::createUniversalCharacterName(value, std::string("\\U") + u8str));
+
+      state = State::End;
+    }
+
+    else if (state == State::SingleLineComment) {
+      u32str = U"//";
+      u32str += curr32;
+      while(1) {
+        if (_u32stream->isEmpty()) {
+          state = State::Error;
+          _setError(R"(Reached end-of-file in single-line-comment)");
+          break;
+        }
+
+        const char32_t ch32 = _u32stream->getChar32();
+        if (ch32 == U'\n') {
+          _queue.push(PPCodeUnit::createComment(u32str));
+          state = State::End;
+          break;
+        } else {
+          u32str += ch32;
+          _u32stream->toNext();
+        }
+      }
+    }
+
+    else if (state == State::MultipleLineCommentStart) {
+      // *      =>  MultipleLineComment
+      // other  =>  Error
+      u32str = U"/*";
+      u32str += curr32;
+      state = State::MultipleLineComment;
+    }
+
+    else if (state == State::MultipleLineComment) {
+      // *      =>  MultipleLineCommentStar
+      // other  =>  MultipleLineComment
+      u32str += curr32;
+      if (curr32 == U'*')
+        state = State::MultipleLineCommentStar;
+    }
+
+    else if (state == State::MultipleLineCommentStar) {
+      // *      =>  MultipleLineCommentStar
+      // /      =>  End
+      // other  =>  MultipleLineComment
+      u32str += curr32;
+
+      if (curr32 == U'*') {
+        //state = State::MultipleLineCommentStar;
+      } else if (curr32 == U'/') {
+        state = State::End;
+        _queue.push(PPCodeUnit::createComment(u32str));
+      } else {
+        state = State::MultipleLineComment;
+      }
+    }
+
+    else if (state == State::LeftParenthesis) {
+      // Previous:  <
+      // %      =>  Emit <% as Digraph
+      // :      =>  Emit <: as Digraph
+      // other  =>  Emit U'<' and curr32 as ASCIIChar
+    }
+
+    else if (state == State::Column) {
+      // Previous:  :
+      // >      =>  Emit :> as Digraph
+      // other  =>  Emit U':' and curr32 as ASCIIChar
+    }
+
+    else if (state == State::PercentSign) {
+      // Previous:  %
+      // >      =>  Emit %> as Digraph
+      // :      =>  Emit U'%' and U':' as ASCIIChar, or
+      //            PercentSign2 otherwise, if next32 is U'%'
+      // other  =>  Emit U'%' and curr32 as ASCIIChar
+    }
+
+    else if (state == State::PercentSign2) {
+      // Previous:  %:%
+      // :      =>  Emit %:%: as Digraph
+      // other  =>  Emit U'%', U':', U'%', and curr32 as ASCIIChar
+      //
+      // For example: %:%d is converted to {'%', ':', '%', 'd'}, not {'%:', '%',
+      // 'd'}.
+    }
+
+    else if (state == State::PercentSign3) {
+      // Previous:  %:%
+      // :      =>  Emit %:%: as Digraph
+      // other  =>  Emit U'%', U':', and curr32 as ASCIIChar
+    }
+
+    else if (state == State::End) {
+      state = State::Error;
+      _setError(R"(Reached an impossible state State::End)");
+    }
+
+    else if (state == State::Error) {
+      state = State::Error;
+      _setError(R"(Reached an impossible state State::Error)");
+    }
+
   } // while
 }
--- pa1/PPCodeUnitStream.h
+++ pa1/PPCodeUnitStream.h
@@ -4,6 +4,8 @@
 #include "PPCodeUnit.h"
 #include "UTF32StreamIfc.h"
 #include "PPCodeUnitStreamIfc.h"
+#include <queue>
+#include <string>
 
 class PPCodeUnitStream: public PPCodeUnitStreamIfc {
 public:
@@ -16,11 +18,28 @@ public:
   std::string getErrorMessage() const;
 
 private:
+  std::shared_ptr<UTF32StreamIfc> _u32stream;
+
+  // Error reporting related stuff.
   void _setError(const std::string&&);
   void _clearError();
-  std::shared_ptr<UTF32StreamIfc> _u32stream;
-  std::shared_ptr<PPCodeUnit> _curr;
   std::string _errorMessage;
+
+  // _pushCodeUnits() parses the input stream and pushes one or more PPCodeUnits
+  // to _queue, depending on the content being parsed. Used by toNext() in the
+  // following way: The toNext() function simply pops an element from _queue. If
+  // _queue is empty after poping an element, toNext() calls _pushCodeUnits() to
+  // try replenishing the _queue.
+  //
+  //   Unless isEmpty() evaluates to true, the _queue always has at least one
+  // element outside of the scope of the toNext() function, so that a)
+  // getCodeUnit() has a super simple form, and b) the actual parsing of the
+  // input stream is delayed as much as possible.
+  //
+  //   When isEmpty() evaluates to true iff. both the input stream and the
+  // _queue are empty.
+  void _pushCodeUnits();
+  std::queue<std::shared_ptr<PPCodeUnit>> _queue;
 };
 
 #endif /* end of include guard */
